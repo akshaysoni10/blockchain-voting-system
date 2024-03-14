3abec72ba7e003a8938320772f3e1da70ca57c7e
Akshay Soni <soniakshay17738@gmail.com>
2024-03-14 15:20:18 +0530

    code updated

---
+++ client/src/components/CandidateList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CandidateList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CandidateList
---
+++ client/src/components/VoterList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VoterList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VoterList
